21aedbb14fc055687b0bc51c3ca18461b82253b0
stepan <step.rus.f@gmail.com>
2022-09-14 20:16:09 +0300

    [SP-29] Create model Article

--- server/DataLouna/LounaAdmin/models.py
+++ server/DataLouna/LounaAdmin/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    text = models.TextField(max_length=255)
+    text = models.TextField()
     photo = models.ImageField(blank=True)
 
