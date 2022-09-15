d8e83a41c10fc6faf40a233d4548c39b35d91cf5
stepan <step.rus.f@gmail.com>
2022-09-15 20:54:55 +0300

    [SP-52] create method getArticlesByKeyWords

--- server/DataLouna/LounaAdmin/urls.py
+++ server/DataLouna/LounaAdmin/urls.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from .views import RedirectToAdmin,getArticles,getChildren,getNode
+from .views import RedirectToAdmin,getArticles,getChildren,getNode,getArticlesByKeyWords
 
 app_name = 'LounaAdmin'
 urlpatterns = [
     path('',RedirectToAdmin),
     path('api/getArticles',getArticles),
     path('api/getChildren',getChildren.as_view({'get': 'list'})),
-    path('api/getNode',getNode.as_view({'get':'list'}))
+    path('api/getNode',getNode.as_view({'get':'list'})),
+    path('api/getArticlesByKeyWords',getArticlesByKeyWords.as_view({'get':'list'}))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/DataLouna/LounaAdmin/views.py
+++ server/DataLouna/LounaAdmin/views.py
@@ -4,7 +4,7 @@ from rest_framework import status, viewsets, exceptions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from .models import Article,CategoryNode
+from .models import Article,CategoryNode,Keywords,Keyword_Article
 from .serializer import NodeSerializer
 
 def RedirectToAdmin(request):
@@ -20,7 +20,6 @@ def getArticles(request):
         return Response(instance)
     except IndexError:
         return Response({'getArticle_Error':'ID not found'},status=status.HTTP_400_BAD_REQUEST)
-
 
 
 
@@ -48,5 +47,17 @@ class getNode(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
+class getArticlesByKeyWords(viewsets.ViewSet):
+    def list(self, request):
+        word = self.request.query_params.get('word')
+        id_by_word = Keywords.objects.filter(text=word).values('id')[0]
+        print(f"{id_by_word=}")
+        keyword_A = Keyword_Article.objects.filter(keywords_id=id_by_word['id']).values('article_id')
+        print(f"{keyword_A=}")
+        print(len(keyword_A))
+        article_array = []
+        for i in range (len(keyword_A)):
+            Articles = Article.objects.filter(id=keyword_A[i]['article_id']).values()[0]
+            article_array.append(Articles)
+        return Response(article_array)
 
